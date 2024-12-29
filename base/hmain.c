46c7bf9ecd2993d9f7c51d004457a910c3239c07
宅の士 <HouseMen@foxmail.com>
2024-12-29 08:12:28 +0800

    修复历史PID文件残留碰撞其他软件当前PID时signal_handle误判错误

--- base/hmain.c
+++ base/hmain.c
@@ -11,6 +11,10 @@
 #define environ (*_NSGetEnviron())
 #endif
 
+#ifdef OS_UNIX
+#include <sys/file.h>
+#endif
+
 static void printf_callback(const char* fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -98,22 +102,22 @@ int main_ctx_init(int argc, char** argv) {
     snprintf(g_main_ctx.pidfile, sizeof(g_main_ctx.pidfile), "%s/logs/%s.pid", g_main_ctx.run_dir, g_main_ctx.program_name);
     snprintf(g_main_ctx.logfile, sizeof(g_main_ctx.logfile), "%s/logs/%s.log", g_main_ctx.run_dir, g_main_ctx.program_name);
     hlog_set_file(g_main_ctx.logfile);
-
+    delete_pidfile();
     g_main_ctx.pid = getpid();
     g_main_ctx.oldpid = getpid_from_pidfile();
 #ifdef OS_UNIX
     if (kill(g_main_ctx.oldpid, 0) == -1 && errno == ESRCH) {
         g_main_ctx.oldpid = -1;
     }
-#else
+#endif
 
-    HANDLE hproc = OpenProcess(PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION, FALSE, g_main_ctx.oldpid);
+#ifdef OS_WIN
     DWORD exitCode = 0;
+    const HANDLE hproc = OpenProcess(PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION, FALSE, g_main_ctx.oldpid);
     if (hproc) {
         GetExitCodeProcess(hproc, &exitCode);
         CloseHandle(hproc);
     }
-
     if(exitCode != STILL_ACTIVE) g_main_ctx.oldpid = -1;
 #endif
 
@@ -446,16 +450,23 @@ void setproctitle(const char* fmt, ...) {
 }
 #endif
 
+static FILE* s_fp = NULL;
 int create_pidfile() {
-    FILE* fp = fopen(g_main_ctx.pidfile, "w");
-    if (fp == NULL) {
+    s_fp = fopen(g_main_ctx.pidfile, "w");
+    if (s_fp == NULL) {
         hloge("fopen('%s') error: %d", g_main_ctx.pidfile, errno);
         return -1;
     }
-
+#ifdef OS_UNIX
+    if (flock(fileno(s_fp), LOCK_EX | LOCK_NB) < 0) {
+        hloge("flock('%s') error: %d", g_main_ctx.pidfile, errno);
+        fclose(s_fp);
+        return -1;
+    }
+#endif
     g_main_ctx.pid = hv_getpid();
-    fprintf(fp, "%d\n", (int)g_main_ctx.pid);
-    fclose(fp);
+    fprintf(s_fp, "%d\n", (int)g_main_ctx.pid);
+    fflush(s_fp);
     hlogi("create_pidfile('%s') pid=%d", g_main_ctx.pidfile, g_main_ctx.pid);
     atexit(delete_pidfile);
     return 0;
@@ -463,6 +474,13 @@ int create_pidfile() {
 
 void delete_pidfile(void) {
     hlogi("delete_pidfile('%s') pid=%d", g_main_ctx.pidfile, g_main_ctx.pid);
+    if(s_fp) {
+#ifdef OS_UNIX
+        flock(fileno(s_fp), LOCK_UN);
+#endif
+        fclose(s_fp);
+        s_fp = NULL;
+    }
     remove(g_main_ctx.pidfile);
 }
 
