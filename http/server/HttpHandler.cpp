283e4b2ed10c82cd2ea4ee2bcfd518d7e390bc52
宅の士 <HouseMen@foxmail.com>
2026-06-11 15:15:44 +0800

    未PR同步特有变更项     CMakeList         去除Unix环境编译静态库输出_static后缀         规范化Linux环境输出共享库SONAME     base/hlog         优化Release模式日志宏过度暴露         修复atexit销毁默认日志多线程缺陷         打开日志增加FD_CLOEXEC屏蔽句柄默认继承问题     base/hmain         修复main_ctx_init在特殊情况下误判旧pid运行的问题         支持main_ctx_init自定义设置run_dir     base/hbase         添加hv_normalize_path函数到hbase.h中     http/server/HttpHandler         默认隐藏HTTP-Server头信息         支持http合法相对路径请求         优化http服务器路径文件缓存映射键唯一性         修复误报'/..file'正常路径错误         改进与http服务请求路径相关的一些缺陷     http/server/HttpServer         支持http服务器绑定onAccept和onClose回调         优化无阻塞启动调用http_server_stop多进程模式主程序退出问题     cpputil/hfile         Unix环境open增加FD_CLOEXEC屏蔽句柄默认继承问题     File changes         删除hconfig.h自动生成的易变文件         添加个人CMakePresets便捷CMake编译配置         添加个人install.bat批量快捷编译脚本

--- http/server/HttpHandler.cpp
+++ http/server/HttpHandler.cpp
@@ -42,6 +42,7 @@ HttpHandler::HttpHandler(hio_t* io) :
     tid(0),
     // for http
     io(io),
+    server(NULL),
     service(NULL),
     api_handler(NULL),
     // for websocket
@@ -438,7 +439,7 @@ void HttpHandler::handleExpect100() {
 void HttpHandler::addResponseHeaders() {
     HttpResponse* pResp = resp.get();
     // Server:
-    pResp->headers["Server"] = "libhv/" HV_VERSION_STRING;
+    // pResp->headers["Server"] = "libhv/" HV_VERSION_STRING;
 
     // Connection:
     pResp->headers["Connection"] = keepalive ? "keep-alive" : "close";
@@ -556,14 +557,15 @@ int HttpHandler::defaultRequestHandler() {
 int HttpHandler::defaultStaticHandler() {
     // file service
     std::string path = req->Path();
-    const char* req_path = path.c_str();
-    // path safe check
-    if (req_path[0] != '/' || strstr(req_path, "/..") || strstr(req_path, "\\..")) {
+    path.resize(hv_normalize_path(const_cast<char*>(path.c_str())));
+    if (path.empty()) {
+        hloge("[%s:%d] Illegal relative path: %s", ip, port, req->path.c_str());
         return HTTP_STATUS_BAD_REQUEST;
     }
 
+    const char* req_path = path.c_str();
     std::string filepath;
-    bool is_dir = path.back() == '/' &&
+    const bool is_dir = path.back() == '/' &&
                   service->index_of.size() > 0 &&
                   hv_strstartswith(req_path, service->index_of.c_str());
     if (is_dir) {
